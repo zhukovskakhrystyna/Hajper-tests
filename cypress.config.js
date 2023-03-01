761f45e3625bb7e382a636a359b6f23eecf1c79f
Khrystyna Zhukovska <zhukovska.khrystyna@gmail.com>
2023-03-01 13:42:29 +0200

    second commit

---
+++ cypress.config.js
@@ -0,0 +1,39 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 20000,
+  chromeWebSecurity: false,
+  pageLoadTimeout: 20000,
+  video: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  reporter: "cypress-multi-reporters",
+
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      quite: true,
+      overwrite: true,
+      html: true,
+      json: false,
+      video: true,
+    },
+  },
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "https://www.hajper.com/",
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+});
